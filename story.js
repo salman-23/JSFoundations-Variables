5d498d26d4d0e826ba487ac15a3b6b6d8c073e7d
Sadiq <salmansoor@localhost.localdomain>
2021-01-10 13:04:16 +0300

    variables

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Sadeq";
+let age = 35;
+let hobby = "Reading";
+
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
